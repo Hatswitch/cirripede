cc09746fc4f3e92b119add1aebafe441480a26fa
Giang Nguyen <nguyen59@illinois.edu>
2011-04-19 21:19:35 +0000

    more work on the register program.
    git-svn-id: https://subversion.cs.illinois.edu/svn/hatswitch/research/nguyen59/stego/tools@4023 b46c1d9a-aabe-4910-9b08-2ad125cc3bdd

--- register/register.c
+++ register/register.c
@@ -15,9 +15,12 @@
 #include <openssl/rand.h>
 #include <arpa/inet.h>
 #include <assert.h>
+#include "openssl/ssl.h"
+#include "openssl/err.h"
+#include "curve25519-20050915/curve25519.h"
 
 static const char Id[] =
-    "$Id$";
+  "$Id$";
 
 // Packet length
 #define PCKT_LEN 8192
@@ -51,6 +54,139 @@ struct tcpheader {
   unsigned short int tcph_urgptr;
 };
 
+#define CURVE25519_KEYSIZE (32)
+
+#define safe_free(ptr)                          \
+  do {                                          \
+    if ((ptr)) {                                \
+      free(ptr);                                \
+      ptr = NULL;                               \
+    }                                           \
+  }                                             \
+  while (0)
+
+#define openssl_safe_free(TYPE, ptr)            \
+  do {                                          \
+    if ((ptr)) {                                \
+      TYPE##_free(ptr);                         \
+      ptr = NULL;                               \
+    }                                           \
+  }                                             \
+  while (0)
+
+#define bail_error(err)                                                 \
+  do {                                                                  \
+    if ((err)) {                                                        \
+      printf("error at %s:%d, %s()\n", __FILE__, __LINE__, __func__);   \
+      goto bail;                                                        \
+    }                                                                   \
+  }                                                                     \
+  while (0)
+
+#define bail_null(ptr)                                                  \
+  do {                                                                  \
+    if (NULL == (ptr)) {                                                \
+      printf("NULL error at %s:%d, %s()\n", __FILE__, __LINE__, __func__); \
+      goto bail;                                                        \
+    }                                                                   \
+  }                                                                     \
+  while (0)
+
+#define bail_null_msg(ptr, msg)                                         \
+  do {                                                                  \
+    if (NULL == (ptr)) {                                                \
+      printf("NULL error at %s:%d, %s(): %s\n", __FILE__, __LINE__, __func__, msg); \
+      goto bail;                                                        \
+    }                                                                   \
+  }                                                                     \
+  while (0)
+
+#define bail_require(cond)                                              \
+  do {                                                                  \
+    if (!(cond)) {                                                      \
+      printf("error condition at %s:%d, %s()\n", __FILE__, __LINE__, __func__); \
+      goto bail;                                                        \
+    }                                                                   \
+  }                                                                     \
+  while (0)
+
+#define bail_require_msg(cond, msg)                                     \
+  do {                                                                  \
+    if (!(cond)) {                                                      \
+      printf("error condition at %s:%d, %s(): %s\n", __FILE__, __LINE__, __func__, msg); \
+      goto bail;                                                        \
+    }                                                                   \
+  }                                                                     \
+  while (0)
+
+void gensecretkey(unsigned char secret[CURVE25519_KEYSIZE])
+{
+  int i = 0;
+  for (; i < CURVE25519_KEYSIZE; i++) {
+    if (1 != RAND_bytes(secret + i, 1)) {
+      puts("error generating random bytes for secret key");
+      exit(-1);
+    }
+  }
+  secret[0] &= 248;
+  secret[31] &= 127;
+  secret[31] |= 64;
+}
+
+void computepublickey(unsigned char pub[CURVE25519_KEYSIZE], const unsigned char secret[CURVE25519_KEYSIZE])
+{
+  static const unsigned char basepoint[CURVE25519_KEYSIZE] = {9};
+  curve25519(pub,secret,basepoint);
+}
+
+int
+getciphertext(const u_char sharedcurvekey[CURVE25519_KEYSIZE],
+              u_char ciphertext[4])
+{
+  int err = 0;
+  /// generate key and iv for cipher
+  u_char cipherkey[EVP_MAX_KEY_LENGTH] = {0};
+  u_char cipheriv[EVP_MAX_IV_LENGTH] = {0};
+
+  // data to derive cipher key/iv
+  u_char kdf_data[CURVE25519_KEYSIZE + 1] = {0};
+  // use the shared curve25519 key
+  memcpy(kdf_data, sharedcurvekey, sizeof sharedcurvekey);
+  // and the last byte is for now hardcoded "1"
+  kdf_data[(sizeof kdf_data) - 1] = '1';
+
+  const EVP_CIPHER *cipher = EVP_aes_128_cbc();
+  int keylen = EVP_BytesToKey(
+    cipher, EVP_sha1(), NULL, kdf_data, sizeof kdf_data, 1,
+    cipherkey, cipheriv);
+  bail_require(keylen == cipher->key_len);
+
+  BIO *benc = BIO_new(BIO_f_cipher());
+  bail_null(benc);
+
+  BIO_set_cipher(benc, cipher, cipherkey, cipheriv, 1);
+
+  BIO *ciphertextbio = BIO_new(BIO_s_mem());
+  bail_null(ciphertextbio);
+
+  bail_require(BIO_push(benc, ciphertextbio) == benc);
+
+  // encrypt "hello"
+  const char str[] = "hello";
+  int retval = BIO_write(benc, str, strlen(str));
+  bail_require(retval == strlen(str));
+  bail_require(1 == BIO_flush(benc));
+
+  // read out the first 4 bytes of the ciphertext
+  retval = BIO_read(ciphertextbio, ciphertext, sizeof ciphertext);
+  bail_require(retval == sizeof ciphertext);
+
+bail:
+  openssl_safe_free(BIO, benc);
+  openssl_safe_free(BIO, ciphertextbio);
+  return err;
+}
+
 int main(int argc, char *argv[])
 {
   int sd;
@@ -64,12 +200,33 @@ int main(int argc, char *argv[])
 
   memset(buffer, 0, PCKT_LEN);
 
-  if(argc != 3)
+  if(argc != 4)
   {
     printf("- Invalid parameters!!!\n");
-    printf("- Usage: %s <target IP> <target port>\n", argv[0]);
+    printf("- Usage: %s <target IP> <target port> <RS curve25519 pubkey file>\n", argv[0]);
+    printf("  - RS is 'registration server'\n");
     exit(-1);
   }
+
+  BIO *rspubkeybio = BIO_new_file(argv[3], "rb");
+  bail_null_msg(rspubkeybio, "can't open RS pubkey file");
+
+  u_char rspubkey[CURVE25519_KEYSIZE] = {0};
+  bail_require_msg(BIO_read(rspubkeybio, rspubkey, sizeof rspubkey) != sizeof rspubkey,
+                   "error reading RS pubkey");
+
+  u_char myseckey[CURVE25519_KEYSIZE] = {0};
+  gensecretkey(myseckey);
+
+  u_char sharedkey[CURVE25519_KEYSIZE] = {0};
+  curve25519(sharedkey, myseckey, rspubkey);
+
+  u_char ciphertext[4] = {0};
+  bail_error(getciphertext(sharedkey, ciphertext));
+
+
+
+  /// handle the socket stuff
 
   sd = socket(PF_INET, SOCK_RAW, IPPROTO_TCP);
 
@@ -142,7 +299,8 @@ int main(int argc, char *argv[])
     sleep(0.1);
   }
 
+bail:
   close(sd);
-
+  openssl_safe_free(BIO, rspubkeybio);
   return 0;
 }
