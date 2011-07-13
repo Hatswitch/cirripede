89d9143aed5a8f390a7e046283cceeab0ca40656
Giang Nguyen <nguyen59@illinois.edu>
2011-07-13 21:46:02 +0000

    python script to process output of countpackets.cc
    git-svn-id: https://subversion.cs.illinois.edu/svn/hatswitch/research/nguyen59/stego/tools@4610 b46c1d9a-aabe-4910-9b08-2ad125cc3bdd

---
+++ misc/sumpackets.py
@@ -0,0 +1,45 @@
+import re
+
+'''
+process output of countpackets.cc
+'''
+
+Id = '$Id$'
+
+d = {'SYN': [0, 0],
+     '443': [0, 0],
+     }
+
+def run(fpath):
+    f=open(fpath)
+    pattern = re.compile(
+        r'(SYN|443): [0-9.]+ M \(([0-9]+)\), [0-9.]+ GB \(([0-9]+) bytes\)')
+    for l in f.readlines():
+        if l.startswith('SYN:') or l.startswith('443:'):
+            l = l.strip()
+            match = re.match(pattern, l)
+            assert match
+            print l
+            print match.groups()
+            typ = match.group(1)
+            pktcount = int(match.group(2))
+            size = int(match.group(3))
+            d[typ] [0] += pktcount
+            d[typ] [1] += size
+            print
+            pass
+        pass
+
+    f.close()
+
+    print 'file:', fpath
+    for k in ('SYN', '443'):
+        v = d[k]
+        print k, ':', float(v[0])/(10**6), 'M packets, ', float(v[1])/(2**30), 'GB'
+        pass
+    return
+
+if __name__ == '__main__':
+    import sys
+    run(sys.argv[1])
+    pass
