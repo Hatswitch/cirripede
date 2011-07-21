2cdc642f3119666cbf7366fe7de65d80e6fd0438
Giang Nguyen <nguyen59@illinois.edu>
2011-07-21 03:47:49 +0000

    add script to process output of measureSYNInterarrival.cc
    git-svn-id: https://subversion.cs.illinois.edu/svn/hatswitch/research/nguyen59/stego/tools@4642 b46c1d9a-aabe-4910-9b08-2ad125cc3bdd

---
+++ misc/processSYNInterarrivalOutput.py
@@ -0,0 +1,55 @@
+import re
+
+'''
+process output of measureSYNInterarrival.cc
+'''
+
+Id = '$Id$'
+
+####################################################################
+def genCDFFromCounts(valueToCounts, outFilePath):
+    totalCount = sum(valueToCounts.values())
+    fil = open(outFilePath, 'w')
+    fil.write('# total count: %u\n' % (totalCount))
+    cumulativeCount = 0
+    for value in sorted(valueToCounts.keys()):
+        cumulativeCount += valueToCounts[value]
+        fraction = float(cumulativeCount) / totalCount
+        fil.write('%f\t%f  # count of this value: %u\n' % (value, fraction, valueToCounts[value]))
+        pass
+    fil.close()
+    return
+
+####################################################################
+
+d = {}
+
+def run(fpath):
+    f=open(fpath)
+    pattern = re.compile(
+        r'interval: ([0-9]+), count: ([0-9]+)')
+    for l in f.readlines():
+        l = l.strip()
+        match = re.match(pattern, l)
+        if match:
+#            print match.groups()
+            interval = int(match.group(1))
+            pktcount = int(match.group(2))
+            if not interval in d:
+                d[interval] = 0
+                pass
+            d[interval] += 1
+#            print
+            pass
+        pass
+
+    f.close()
+
+    print 'file:', fpath
+    genCDFFromCounts(d, 'cdf_syniat')
+    return
+
+if __name__ == '__main__':
+    import sys
+    run(sys.argv[1])
+    pass
