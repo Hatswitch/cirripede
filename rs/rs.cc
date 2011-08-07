40f0dd9f0188ae9150e9db190687b618ee88f375
Giang Nguyen <nguyen59@illinois.edu>
2011-08-07 02:51:33 +0000

    rs uses some openssl stuff. not sure if these things require thread-safety, but openssl doc says: "Note that OpenSSL uses a number of global data structures that will be implicitly shared whenever multiple threads use OpenSSL." so we make rs openssl thread-safe just in case.
    git-svn-id: https://subversion.cs.illinois.edu/svn/hatswitch/research/nguyen59/stego/tools@4759 b46c1d9a-aabe-4910-9b08-2ad125cc3bdd

--- rs/rs.cc
+++ rs/rs.cc
@@ -316,6 +316,81 @@ struct sniff_tcp {
 
 #define CURVE25519_KEYSIZE (32)
 
+/* this part is necessary for openssl to be thread-safe. copied from
+ * openssl's mttest.c
+ */
+
+static pthread_mutex_t *lock_cs;
+static long *lock_count;
+
+unsigned long pthreads_thread_id(void)
+{
+	unsigned long ret;
+
+	ret=(unsigned long)pthread_self();
+	return(ret);
+}
+
+void pthreads_locking_callback(int mode, int type, const char *file,
+                               int line)
+{
+#ifdef undef
+	fprintf(stderr,"thread=%4d mode=%s lock=%s %s:%d\n",
+            CRYPTO_thread_id(),
+            (mode&CRYPTO_LOCK)?"l":"u",
+            (type&CRYPTO_READ)?"r":"w",file,line);
+#endif
+/*
+  if (CRYPTO_LOCK_SSL_CERT == type)
+  fprintf(stderr,"(t,m,f,l) %ld %d %s %d\n",
+  CRYPTO_thread_id(),
+  mode,file,line);
+*/
+	if (mode & CRYPTO_LOCK)
+    {
+		pthread_mutex_lock(&(lock_cs[type]));
+		lock_count[type]++;
+    }
+	else
+    {
+		pthread_mutex_unlock(&(lock_cs[type]));
+    }
+}
+
+void thread_setup(void)
+{
+	int i;
+
+	lock_cs=(pthread_mutex_t*)OPENSSL_malloc(CRYPTO_num_locks() * sizeof(pthread_mutex_t));
+	lock_count=(long *)OPENSSL_malloc(CRYPTO_num_locks() * sizeof(long));
+	for (i=0; i<CRYPTO_num_locks(); i++)
+    {
+		lock_count[i]=0;
+		pthread_mutex_init(&(lock_cs[i]),NULL);
+    }
+
+	CRYPTO_set_id_callback((unsigned long (*)())pthreads_thread_id);
+	CRYPTO_set_locking_callback(pthreads_locking_callback);
+}
+
+void thread_cleanup(void)
+{
+	int i;
+
+	CRYPTO_set_locking_callback(NULL);
+	fprintf(stderr,"cleanup\n");
+	for (i=0; i<CRYPTO_num_locks(); i++)
+    {
+		pthread_mutex_destroy(&(lock_cs[i]));
+		fprintf(stderr,"%8ld:%s\n",lock_count[i],
+                CRYPTO_get_lock_name(i));
+    }
+	OPENSSL_free(lock_cs);
+	OPENSSL_free(lock_count);
+
+	fprintf(stderr,"done cleanup\n");
+}
+
 /*
  * print data in rows of 16 bytes: offset   hex   ascii
  *
