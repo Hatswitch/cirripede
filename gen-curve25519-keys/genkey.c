5431613c5c7195cd16cee586182b71ad5fd275de
Giang Nguyen <nguyen59@illinois.edu>
2011-08-09 00:35:55 +0000

    update genkeys to generate keys with basepoints 6 and 3.
    git-svn-id: https://subversion.cs.illinois.edu/svn/hatswitch/research/nguyen59/stego/tools@4782 b46c1d9a-aabe-4910-9b08-2ad125cc3bdd

--- gen-curve25519-keys/genkey.c
+++ gen-curve25519-keys/genkey.c
@@ -4,24 +4,42 @@
 #include "openssl/rand.h"
 #include "curve25519-20050915/curve25519.h"
 
-void gensecretkey(unsigned char secret[32])
+#define CURVE25519_KEYSIZE (32)
+
+void gensecretkey(unsigned char secret[CURVE25519_KEYSIZE])
 {
   int i = 0;
-  for (; i < 32; i++) {
+  for (; i < CURVE25519_KEYSIZE; i++) {
     if (1 != RAND_bytes(secret + i, 1)) {
       puts("error generating random bytes for secret key");
       exit(-1);
     }
   }
-  secret[0] &= 248;
-  secret[31] &= 127;
-  secret[31] |= 64;
+  /* secret[0] &= 248; */
+  secret[31] &= 127; /* keep this line */
+  /* secret[31] |= 64; */
 }
 
-void computepublickey(unsigned char pub[32], const unsigned char secret[32])
+/* return 0 on success */
+int computepublickey(unsigned char pub[CURVE25519_KEYSIZE],
+                     const unsigned char secret[CURVE25519_KEYSIZE],
+                     const int basepoint /* either 6 or 3 */
+  )
 {
-  static const unsigned char basepoint[32] = {9};
-  curve25519(pub,secret,basepoint);
+  if (basepoint == 6) {
+    static const unsigned char basepoint6[CURVE25519_KEYSIZE] = {6};
+    curve25519(pub,secret, basepoint6);
+    return 0;
+  }
+  else if (basepoint == 3) {
+    static const unsigned char basepoint3[CURVE25519_KEYSIZE] = {3};
+    curve25519(pub,secret, basepoint3);
+    return 0;
+  }
+  else {
+    printf("basepoint must be 6 or 3\n");
+    return 1; // error
+  }
 }
 
 void usage(const char *progname)
@@ -46,38 +64,47 @@ int main(int argc, char *argv[])
     return -1;
   }
 
-  unsigned char mysecret[32] = {0};
-  unsigned char mypublic[32] = {0};
-  gensecretkey(mysecret);
-  computepublickey(mypublic, mysecret);
+  static const int basepoints[] = {6, 3};
+  int i = 0;
+  for (i = 0; i < sizeof (basepoints) / sizeof(basepoints[0]); ++i) {
+    const int basepoint = basepoints[i];
 
-  FILE* file;
-  size_t numwritten;
+    unsigned char mysecret[32] = {0};
+    unsigned char mypublic[32] = {0};
+    gensecretkey(mysecret);
+    computepublickey(mypublic, mysecret, basepoint);
 
-  file = fopen("secret", "wb");
-  if (!file) {
-    puts("can't open secret file");
-    return -1;
-  }
-  numwritten = fwrite(mysecret, 1, sizeof mysecret, file);
-  if (numwritten != sizeof mysecret) {
-    puts("error writing to secret file");
-    return -1;
-  }
-  fclose(file);
+    FILE* file;
+    size_t numwritten;
 
-  file = fopen("public", "wb");
-  if (!file) {
-    puts("can't open public file");
-    return -1;
-  }
-  numwritten = fwrite(mypublic, 1, sizeof mypublic, file);
-  if (numwritten != sizeof mypublic) {
-    puts("error writing to public file");
-    return -1;
-  }
-  fclose(file);
+    char path[80];
+    sprintf(path, "secret-base%u", basepoint);
+    file = fopen(path, "wb");
+    if (!file) {
+      puts("can't open secret file");
+      return -1;
+    }
+    numwritten = fwrite(mysecret, 1, sizeof mysecret, file);
+    if (numwritten != sizeof mysecret) {
+      puts("error writing to secret file");
+      return -1;
+    }
+    fclose(file);
 
-  puts("successful.");
+    sprintf(path, "public-base%u", basepoint);
+    file = fopen(path, "wb");
+    if (!file) {
+      puts("can't open public file");
+      return -1;
+    }
+    numwritten = fwrite(mypublic, 1, sizeof mypublic, file);
+    if (numwritten != sizeof mypublic) {
+      puts("error writing to public file");
+      return -1;
+    }
+    fclose(file);
+
+    puts("successful.");
+  }
   return 0;
 }
